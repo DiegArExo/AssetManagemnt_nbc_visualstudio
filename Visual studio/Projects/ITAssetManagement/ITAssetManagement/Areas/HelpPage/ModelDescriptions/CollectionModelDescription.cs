1e423735187923d9822f0f5de4813f27f6223a78
DiegArExo <cloody1000@gmail.com>
2024-06-28 08:26:25 +0200

    mm

--- Visual studio/Projects/ITAssetManagement/ITAssetManagement/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ITAssetManagement.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- Visual studio/Projects/ITAssetManagement/ITAssetManagement/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace ITAssetManagement.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
